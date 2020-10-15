3c0145bb117fdcf0c2456800b1f20d421aed6242
Albatrooss <robillardtim@gmail.com>
2020-10-15 12:17:28 -0400

    privacy added

---
+++ src/pages/Privacy.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+export default function Privacy() {
+  return (
+    <main className="main-privacy">
+      <h1>Privacy Policy</h1>
+      <p>Your privacy is important to us. It is Works in Progress Movement’s policy to respect your privacy regarding any information we may collect from you across our website, www.worksinprogressmovement.ca.</p>
+      <h2>1. Information we collect</h2>
+      <h4>Log data</h4>
+      <p>When you visit our website, our servers may automatically log the standard data provided by your web browser. It may include your computer’s Internet Protocol (IP) address, your browser type and version, the pages you visit, the time and date of your visit, the time spent on each page, and other details.</p>
+      <h4>Device data</h4>
+      <p>We may also collect data about the device you’re using to access our website. This data may include the device type, operating system, unique device identifiers, device settings, and geo-location data. What we collect can depend on the individual settings of your device and software. We recommend checking the policies of your device manufacturer or software provider to learn what information they make available to us.</p>
+
+      <h2>2. Legal bases for processing</h2>
+      <p>We will process your personal information lawfully, fairly and in a transparent manner. We collect and process information about you only where we have legal bases for doing so.</p>
+      <p>Where you consent to our use of information about you for a specific purpose, you have the right to change your mind at any time (but this will not affect any processing that has already taken place).</p>
+      <p>We don’t keep personal information for longer than is necessary. While we retain this information, we will protect it within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use or modification. That said, we advise that no method of electronic transmission or storage is 100% secure and cannot guarantee absolute data security. </p>
+
+      <h2>3. Collection and use of information</h2>
+      <p>We may collect, hold, and use information to enable you to access and use our website. Personal information will not be further processed in a manner that is incompatible with this purpose.</p>
+
+      <h2>4. Disclosure of personal information to third parties</h2>
+      <p>We will not disclose personal information to third parties.</p>
+
+      <h2>5. International transfers of personal information</h2>
+      <p>The personal information we collect is stored and processed in Canada.</p>
+
+      <h2>6. Your rights and controlling your personal information</h2>
+      <p>Choice and consent: By providing personal information to us, you consent to us collecting, holding, and using your personal information in accordance with this privacy policy. If you are under 16 years of age, you must have, and warrant to the extent permitted by law to us, that you have your parent or legal guardian’s permission to access and use the website and they (your parents or guardian) have consented to you providing us with your personal information. You do not have to provide personal information to us, however, if you do not, it may affect your use of this website or the products and/or services offered on or through it.</p>
+      <p>Notification of data breaches: We will comply laws applicable to us in respect of any data breach.</p>
+      <p>Complaints: If you believe that we have breached a relevant data protection law and wish to make a complaint, please contact us and provide us with full details of the alleged breach. We will promptly investigate your complaint and respond to you, in writing, setting out the outcome of our investigation and the steps we will take to deal with your complaint. You also have the right to contact a regulatory body or data protection authority in relation to your complaint.</p>
+
+      <h2>7. Cookies</h2>
+      <p>A cookie is a small piece of data that our website stores on your computer, and accesses each time you visit our site. This helps us serve you content based on preferences you have specified.</p>
+
+      <h2>8. Limits of our policy</h2>
+      <p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and policies of those sites, and cannot accept responsibility or liability for their respective privacy practices.</p>
+
+      <h2>9. Changes to this policy</h2>
+      <p>At our discretion, we may change our privacy policy to reflect current acceptable practices. We will take reasonable steps to let users know about changes via our website. Your continued use of this site after any changes to this policy will be regarded as acceptance of our practices around privacy and personal information.</p>
+      <p>If we make a significant change to this privacy policy, for example changing a lawful basis on which we process your personal information, we will ask you to re-consent to the amended privacy policy.</p>
+      <p>This policy is effective as of 14 Oct 2020.</p>
+
+      <h2>Works in Progress Movement Terms of Service</h2>
+      <h4>1. Terms</h4>
+      <p>By accessing the website at www.worksinprogressmovement.ca, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. </p>
+      <h4>2. Disclaimer</h4>
+      <p>Works in Progress Movement does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</p>
+      <h4>4. Limitations</h4>
+      <p>In no event shall Works in Progress Movement be liable for any damages arising out of the use or inability to use the materials on Works in Progress Movement’s website. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+      <h4>5. Accuracy of materials</h4>
+      <p>The materials appearing on Works in Progress Movement’s website could include technical, typographical, or photographic errors. Works in Progress Movement does not warrant that any of the materials on its website are accurate, complete or current. Works in Progress Movement may make changes to the materials contained on its website at any time without notice. However Works in Progress Movement does not make any commitment to update the materials.</p>
+      <h4>6. Links</h4>
+      <p>Works in Progress Movement has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Works in Progress Movement or the site. Use of any such linked website is at the user’s own risk.</p>
+      <h4>7. Modifications</h4>
+      <p>Works in Progress Movement may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
+      <h4>8. Governing Law</h4>
+      <p>These terms and conditions are governed by and construed in accordance with the laws of Ontario and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+
+      <h2>Cookie Policy</h2>
+      <p>We use cookies to help improve your experience of worksinprogresmovement.ca. This cookie policy is part of Works in Progress Movement’s privacy policy, and covers the use of cookies between your device and our site. </p>
+      <p>If you don’t wish to accept cookies from us, you should instruct your browser to refuse cookies from www.worksinprogressmovement.ca, with the understanding that we may be unable to provide you with some of your desired content and services.</p>
+      <h4>What is a cookie?</h4>
+      <p>A cookie is a small piece of data that a website stores on your device when you visit, typically containing information about the website itself, a unique identifier that allows the site to recognise your web browser when you return, additional data that serves the purpose of the cookie, and the lifespan of the cookie itself.</p>
+      <p>Cookies are used to enable certain features (eg. logging in). </p>
+      <p>Cookies set by the website you are visiting are normally referred to as “first-party cookies”, and typically only track your activity on that particular site. Cookies set by other sites and companies (ie. third parties) are called “third-party cookies”, and can be used to track you on other websites that use the same third-party service.</p>
+      <p>Types of cookies and how we use them</p>
+      <ul><li><p>Essential cookies: Essential cookies are crucial to your experience of a website, enabling core features like user logins, account management, shopping carts and payment processing. We use essential cookies to enable certain functions on our website.</p></li></ul>
+      <h4>How you can control or opt out of cookies</h4>
+      <p>If you do not wish to accept cookies from us, you can instruct your browser to refuse cookies from our website. Most browsers are configured to accept cookies by default, but you can update these settings to either refuse cookies altogether, or to notify you when a website is trying to set or update a cookie.</p>
+      <p>If you browse websites from multiple devices, you may need to update your settings on each individual device.</p>
+      <p>Although some cookies can be blocked with little impact on your experience of a website, blocking all cookies may mean you are unable to access certain features and content across the sites you visit.</p>
+
+    </main>
+  )
+}
